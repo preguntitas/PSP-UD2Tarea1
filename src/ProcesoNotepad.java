3d28b85612f89b2c651cd52d041a292177610d3f
Mariano Murcia Ortuño <marmurort@alu.edu.gva.es>
2023-10-20 01:57:18 +0200

    ProcesoNotepad.java

---
+++ src/ProcesoNotepad.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+public class ProcesoNotepad {
+    public static void main(String[] args) {
+        try {
+        Process notepad = new ProcessBuilder("notepad.exe").start();
+        while(notepad.isAlive()){
+            System.out.println("Está vivo!");
+            Thread.sleep(3000);
+            }
+        } catch (IOException e) {
+            System.out.println("Error al ejecutar el proceso: " + e.getMessage());;
+        } catch(InterruptedException e) {
+            System.out.println("EL proceso se ha interrumpido: " + e.getMessage());
+        }
+        System.out.println("Ya no está vivo");
+    }
+}
